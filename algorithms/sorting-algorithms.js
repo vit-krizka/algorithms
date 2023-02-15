8bdc28ae65ca2bf645f1fc216ad3ad10b9336b5b
Vít Křížka <vit.krizka@gmail.com>
2023-02-15 16:07:50 +0100

    SIEVE OF ERATOSTHENES - final

--- algorithms/sorting-algorithms.js
+++ algorithms/sorting-algorithms.js
@@ -7,7 +7,7 @@ console.log(array2.sort());
 
 //Sorted in Ascending Order
 const ascendingOrder = (a, b) => {
-    return a - b;
+  return a - b;
 }
 
 // Use this array to test your code:
@@ -17,25 +17,25 @@ console.log(testArray);
 
 //Sorting by Length
 const sortByLength = (a, b) => {
-    if (a.length < b.length) {
-      return -1;
-    } else if (a.length > b.length) {
-      return 1;
-    } else {
-      return ascendingOrder(a, b);
-    }
+  if (a.length < b.length) {
+    return -1;
+  } else if (a.length > b.length) {
+    return 1;
+  } else {
+    return ascendingOrder(a, b);
+  }
 }
-  
+
 const ascendingOrder = (a, b) => {
-    if (a < b) {
-        return -1;
-    } else if (a > b) {
-        return 1;
-    } else {
-        return 0;
-    }
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
-  
+
 // Use this array to test your code:
 const testArray = ['car', 'train', 'plane', 'bike', 'skateboard', 'jet'];
 testArray.sort(sortByLength);
@@ -43,25 +43,25 @@ console.log(testArray);
 
 //Explicit Ordering
 const explicitSortWithComparator = (inputArray, order) => {
-  
-    const explicitComparator = (a, b) => {
-      let indexA = order.length;
-      let indexB = order.length;
-      if (order.includes(a)) {
-        indexA = order.indexOf(a);
-      }
-      if (order.includes(b)) {
-        indexB = order.indexOf(b);
-      }
-      return indexA - indexB;
+
+  const explicitComparator = (a, b) => {
+    let indexA = order.length;
+    let indexB = order.length;
+    if (order.includes(a)) {
+      indexA = order.indexOf(a);
     }
-    
-    return inputArray.sort(explicitComparator).slice();
+    if (order.includes(b)) {
+      indexB = order.indexOf(b);
+    }
+    return indexA - indexB;
   }
-  
-  // Use this array to test your code:
-  const inputArray = ['a', 'b', 'c', 'd', 'e', 'f', 'n', 'y', 'g']
-  const order = ['a', 'n', 'd', 'y']
-  console.log(explicitSortWithComparator(inputArray, order));
-  
-  module.exports = explicitSortWithComparator;
+
+  return inputArray.sort(explicitComparator).slice();
+}
+
+// Use this array to test your code:
+const inputArray = ['a', 'b', 'c', 'd', 'e', 'f', 'n', 'y', 'g']
+const order = ['a', 'n', 'd', 'y']
+console.log(explicitSortWithComparator(inputArray, order));
+
+module.exports = explicitSortWithComparator;
